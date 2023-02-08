b326a2ac6548ff3e22d330b00d9963fdb0baf4ad
Raskc <afraieda@gmail.com>
2023-02-08 14:24:17 +0100

    add: exo1

---
+++ Syntax.lua
@@ -0,0 +1,9 @@
+for i = 1,100 do
+    if (i % 5 == 0 and i % 3 == 0) then
+        print("ThreeFive")
+    elseif (i % 5 == 0) then
+        print("Five")
+    elseif (i % 3 == 0) then
+        print("Three")
+    end
+end
