6358cbeae1067c7fd480c66213bd259839a94656
Raskc <afraieda@gmail.com>
2023-02-08 15:28:53 +0100

    add: exo7

---
+++ Ex07.lua
@@ -0,0 +1,15 @@
+function Pet(name, status)
+    local module = {}
+    module.name = name or "Kikki"
+    module.status = status or "Hungry"
+
+    function module.check(self)
+        print(string.format("%s is %s", self.name, self.status))
+    end
+
+    function module.feed(self)
+        print(string.format("%s has been fed", self.name))
+    end
+
+    return module
+end
